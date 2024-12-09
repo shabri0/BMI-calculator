4fd4f769c8ea1b7e81558c8cc80dae5b38b4d03d
Shabri <162605343+shabri0@users.noreply.github.com>
2024-12-09 17:22:17 +0530

    Add files via upload

---
+++ BMI.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float weight, height,bmi;
+    cout << "Weight (kg), height(m): ";
+    cin >> weight >> height;
+    bmi = weight/(height*height);
+    if(bmi < 18.5)
+    cout << "Underweight" << endl;
+    else if(bmi > 25)
+    cout <<"Overweight"<<endl;
+    else
+    cout << "Normal weight" << endl;
+    cout << "Your BMI is " <<bmi;
+
+    return 0;
+}
